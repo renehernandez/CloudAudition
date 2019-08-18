bc7226ffc66f0adf0b9e66c28b4249758dc390b8
Rene Hernandez <renehr9102@gmail.com>
2019-08-18 11:55:43 -0400

    CA-1 Remove check for file dot sourcing

--- build.ps1
+++ build.ps1
@@ -9,43 +9,39 @@ param (
     $UseDocker
 )
 
-$isDotSourced = $MyInvocation.InvocationName -eq '.' -or $MyInvocation.Line -eq ''
 
 $isDockerPresent = (Get-Command -Name 'docker*' | Measure-Object).Count -ne 0
 
-if (-not $isDotSourced) {
-    Write-Host "Start pipeline execution"
+Write-Host "Start pipeline execution"
 
-    $PSDependenciesPath = Join-Path -Path $PSScriptRoot -ChildPath 'PSDependencies'
-    if (-not (Test-Path -Path $PSDependenciesPath)) {
-        New-Item -Path $PSDependenciesPath -ItemType Directory -Force | Out-Null
-    }
+$PSDependenciesPath = Join-Path -Path $PSScriptRoot -ChildPath 'PSDependencies'
+if (-not (Test-Path -Path $PSDependenciesPath)) {
+    New-Item -Path $PSDependenciesPath -ItemType Directory -Force | Out-Null
+}
 
-    if (-not (Test-Path -Path $PSDependenciesPath/InvokeBuild)) {
-        Save-Module -Name InvokeBuild -Path $PSDependenciesPath -Force
-    }
+if (-not (Test-Path -Path $PSDependenciesPath/InvokeBuild)) {
+    Save-Module -Name InvokeBuild -Path $PSDependenciesPath -Force
+}
 
-    Import-Module -Name $PSDependenciesPath/InvokeBuild -Force
+Import-Module -Name $PSDependenciesPath/InvokeBuild -Force
 
-    $params = @{
-        Task = $Task
-        Result = 'Result'
-    }
+$params = @{
+    Task = $Task
+    Result = 'Result'
+}
 
-    $buildProjectFiles = Get-ChildItem -Path $PSScriptRoot -File -Filter '*.build.ps1' -Recurse -Depth 1
+$buildProjectFiles = Get-ChildItem -Path $PSScriptRoot -File -Filter '*.build.ps1' -Recurse -Depth 1
 
-    foreach ($buildFile in $buildProjectFiles) {
-        Write-Host "Start pipeline tasks for $($buildFile.BaseName)"
-        $params.File = $buildFile.FullName
-        Invoke-Build @params
+foreach ($buildFile in $buildProjectFiles) {
+    Write-Host "Start pipeline tasks for $($buildFile.BaseName)"
+    $params.File = $buildFile.FullName
+    Invoke-Build @params
 
-        if ($Result.Error) {
-            foreach ($task in $Result.Tasks) {
-                if ($task.Error) {
-                    Write-Error "Task '$($task.Name)' at $($task.InvocationInfo.ScriptName):$($task.InvocationInfo.ScriptLineNumber)"
-                }
+    if ($Result.Error) {
+        foreach ($task in $Result.Tasks) {
+            if ($task.Error) {
+                Write-Error "Task '$($task.Name)' at $($task.InvocationInfo.ScriptName):$($task.InvocationInfo.ScriptLineNumber)"
             }
         }
     }
 }
-
